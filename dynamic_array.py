260be6e1d109ce533d51e2223b0f001a9b743b7e
Speckeldore <santossk@oregonstate.edu>
2023-02-08 15:04:00 -0800

    hello world

--- dynamic_array.py
+++ dynamic_array.py
@@ -219,22 +219,21 @@ class DynamicArray:
         """
         TODO: Write this implementation
         """
-        # cas for returning just the initializer; there is no array
+        # if there is no array return initializer
         if self._size == 0:
             return initializer
-        # This is the case for there is only 1 element in the array
-        # if self._size == 1 and initializer is not None:
-        #    return reduce_func(initializer, self._data[0])
-        for i in range(0, self._size):
-            if initializer == None:
-                if self._size == 1:
-                    return self._data[0]
-                initializer = self._data[0]
-                i += 1
-                if self._size == 1:
-                    return reduce_func(initializer, 0)
-            value = reduce_func(initializer, self._data[i])
-            initializer = value
+        # if the intializer is none and and the size of the array is 1
+        if initializer == None:
+            if self._size == 1:
+                return self._data[0]
+            value = self._data[0]
+        # if the initializer is a value
+        if initializer is not None:
+            value = initializer
+            value = reduce_func(value, self._data[0])
+
+        for i in range(1, self._size):
+            value = reduce_func(value, self._data[i])
 
         return value
 def find_mode(arr: DynamicArray) -> (DynamicArray, int):
--- insert.py
+++ insert.py
@@ -1,2 +1,21 @@
-for i in range(0,1):
-    print(i)
+def reduce(self, reduce_func, initializer=None) -> object:
+    """
+    TODO: Write this implementation
+    """
+# if there is no array return initializer
+    if self._size == 0:
+        return initializer
+#if the intializer is none and and the size of the array is 1
+    if initializer == None:
+        if self._size == 1:
+            return self._data[0]
+        value = self._data[0]
+#if the initializer is a value
+    if initializer is not None:
+        value = initializer
+        value = reduce_func(value, self._data[0])
+
+    for i in range(1, self._size):
+        value = reduce_func(value, self._data[i])
+
+    return value
