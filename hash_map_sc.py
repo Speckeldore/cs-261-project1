43122f79aba4edd071af6f4beb9e6b6d5914f3ed
Speckeldore <santossk@oregonstate.edu>
2023-03-10 12:09:21 -0800

    hello worldd

--- hash_map_sc.py
+++ hash_map_sc.py
@@ -126,7 +126,7 @@ class HashMap:
         if new_capacity < 1:
             return
         if new_capacity < self._size:
-            new_capacity = self._size:
+            new_capacity = self._size
             if self._is_prime(new_capacity) is False:
                 new_capacity = self._next_prime(new_capacity)
         if self._is_prime(new_capacity) is False:
