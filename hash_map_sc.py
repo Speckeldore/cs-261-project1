dc95043860a26d834ec8dc4570b23077468dd878
Speckeldore <santossk@oregonstate.edu>
2023-03-13 13:41:18 -0700

    hello worldd

--- hash_map_sc.py
+++ hash_map_sc.py
@@ -77,6 +77,10 @@ class HashMap:
         This method updates the key/value pair in the hash map
         """
         #simply adding the value
+        self._loadF = self._size / self._capacity
+        if self._loadF >= 1:
+            self.resize_table(2 * self._capacity)
+
         LL = self._buckets.get_at_index(self._hash_function(key)%self._capacity)
         # If the given key already exists in the hash map, its associated value must be replaced with the new value
         if LL.contains(key) is not None:
@@ -84,9 +88,6 @@ class HashMap:
         else:
             LL.insert(key,value)
             self._size += 1
-        self._loadF = self._size / self._capacity
-        if self._loadF >= 1:
-            self.resize_table(2 * self._capacity)
         # If the given key is not in the hash map, a new key/value pair must be added.
         # if load factor is greater than 1 double size to next prime number?
             #for _ in range(self._capacity):
